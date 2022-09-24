9b9a4d78753384d8e23d6c276c2d26ae0c244f71
jniac <joseph.merdrignac@gmail.com>
2022-09-24 13:00:16 +0200

    oops, typo ("{}" instead of "void")

--- src/fields/group.ts
+++ src/fields/group.ts
@@ -1,6 +1,6 @@
 import { Group } from '../core/items'
 
-export const group = (path: string, scopeCallback: () => {}) => {
+export const group = (path: string, scopeCallback: () => void) => {
   const group = Group.getOrCreate(path)
   group.within(scopeCallback)
   return group
