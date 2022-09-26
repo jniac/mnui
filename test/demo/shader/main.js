60a1a2a68fd3faa14ce3567abbc890c15b0e8b0b
jniac <joseph.merdrignac@gmail.com>
2022-09-26 23:22:41 +0200

    the shader is cool

--- test/demo/shader/main.js
+++ test/demo/shader/main.js
@@ -2,7 +2,7 @@ import { THREE } from '../src/three/THREE.js'
 import { getPlane } from '../src/three/utils.js'
 import { glsl_utils, glsl_easings, glsl_iq_noise } from '../src/glsl/utils/index.js'
 import { mnui } from '../../../dist/index.js'
-import { camera } from '../src/three/stage.js'
+import { camera, renderer } from '../src/three/stage.js'
 
 
 const vertexShader = /* glsl */`
@@ -37,6 +37,33 @@ ${glsl_utils}
 ${glsl_easings}
 ${glsl_iq_noise}
 
+// https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+
+float noise(vec3 p){
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (3.0 - 2.0 * d);
+
+    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
+
+    vec4 c = k2 + a.zzzz;
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
+
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+    return o4.y * d.y + o4.x * (1.0 - d.y);
+}
+
 vec4 transparent = vec4(0.0, 0.0, 0.0, 0.0);
 vec4 black = vec4(0.0, 0.0, 0.0, 1.0);
 vec4 white = vec4(1.0, 1.0, 1.0, 1.0);
@@ -48,8 +75,11 @@ uniform float uTime;
 uniform vec4 uSize;
 uniform vec4 uParam1;
 uniform vec4 uTransform1;
-uniform vec4 uShadowLength;
+uniform vec4 uShadow;
+uniform vec4 uOffset1;
 uniform float uAliasThreshold;
+uniform float uZoom;
+uniform vec3 uColor;
 
 varying vec3 vPosition;
 varying vec4 vColor;
@@ -68,39 +98,76 @@ vec2 applyTransform2D(vec2 p, vec4 transform) {
   return p;
 }
 
-float checker(vec2 uv, float size) {
+bool checker(vec2 uv, float size) {
   float x = positiveModulo(uv.x, size * 2.0);
   float y = positiveModulo(uv.y, size * 2.0);
-  return x < size == y < size ? 1.0 : 0.0;
+  return x < size == y < size;
 }
 
-float sinRamp(vec2 uv) {
-  float x1 = (uv.y * 8.0);
-  float x2= x1 - sin(uv.x * 8.0) * 0.5 + sin(uv.x * 2.0) * 0.5;
+// Why returning a vec2 here instead of a float? Is it a distance or not?
+// Yes, but a special one that has 2 values, a "flat" distance, "strait", which
+// represents a distance from a strait line, and a "sin" distance, "curve" which
+// represents a distance from a curvy line. The flat distance commands the shadow 
+// on great distance, the sin distance on short distance.
+vec2 sinDistance(vec2 uv) {
+  float strait = -(uv.y * 8.0);
+  float curveBase = strait - 1.0;
+  float curve = curveBase - sin(uv.x * 8.0) * 0.5 + sin(uv.x * 2.0) * 0.5;
+  return vec2(strait, curve);
+}
 
-  float a = fwidth(x1) * 0.5 * uAliasThreshold;
-  x2 /= 4.0;
-
-  if (x2 < a) {
-    return smoothstep(0.0, a, x2);
+float shadowRamp(vec2 dist) {
+  float strait = dist.x;
+  float curve = dist.y;
+  float a = fwidth(strait) * 0.5 * uAliasThreshold;
+  if (curve < a) {
+    return smoothstep(0.0, a, curve);
   } else {
-    x1 = (x1 - a) / (1.0 - a);
-    x2 = (x2 - a) / (1.0 - a);
-    float x3 = easeOut6(x2 / uShadowLength.y); // sin distance
-    float x4 = easeOut3(x1 / uShadowLength.x); // flat distance
-    return clamp01(1.0 - x3 * mix(x4, 1.0, 0.5));
+    strait = (strait - a) / (1.0 - a);
+    curve = (curve - a) / (1.0 - a);
+    float xStr = easeOut6(curve / uShadow.y); // sin distance
+    float xCrv = easeOut3(strait / uShadow.x); // flat distance
+    xCrv = mix(1.0, xCrv, uShadow.z);
+    xStr = mix(1.0, xStr, uShadow.w);
+    return clamp01(1.0 - xStr * xCrv);
   }
 }
 
-void main()	{
+vec2 getUv2(in vec2 uv, in float i) {
+  vec2 uv2 = uv;
+  float z = 0.3 * sin(uTime * 0.1 + uv.x + uv.y);
+  uv2.x += noise(vec3(uv * 0.4 + i * uOffset1.z, z)) + i * uOffset1.x;
+  uv2.y += noise(vec3(uv * 0.4 + i * uOffset1.w, z)) + i * uOffset1.y;
+  return uv2;
+}
 
-  vec2 uv = vPosition.xy * 4.0 * vec2(uSize.z, 1.0) + uParam1.xy;
+float grain(in vec2 uv) {
+  float x = noise(vec3(uv * 1000.1, uTime));
+  return mix(0.9, 1.1, x);
+}
+
+void main()	{
+  vec2 uv = vPosition.xy * uZoom * 4.0 * vec2(uSize.z, 1.0) + uParam1.xy;
   uv = applyTransform2D(uv, uTransform1);
-  uv.x += iq_noise3(vec3(uv * 0.4, 0.0));
-  uv.y += iq_noise3(vec3(uv * 0.4, 0.0));
-  float x = checker(uv, 1.0);
-  gl_FragColor.rgb = vec3(1.0 - sinRamp(uv)) * 0.1;
-  // gl_FragColor.rgb *= x > 0.0 ? 1.0 : 0.97;
+  vec2 uv2 = getUv2(uv, 0.0);
+  vec2 dist = sinDistance(uv2);
+  float shadow = 1.0;
+  shadow *= 1.0 - shadowRamp(dist);
+  vec3 color = vec3(1.0, 0.0, 0.0);
+  for (float i = 0.0; i < 4.0; i += 1.0) {
+    vec2 uv2 = getUv2(uv, i + 1.0);
+    vec2 dist2 = sinDistance(uv2);
+    if (dist.y > 0.0) {
+      float newShadow = 1.0 - shadowRamp(dist2);
+      shadow *= newShadow;
+      // color = mod(i, 2.0) == 1.0 ? vec3(0.0, 0.0, 1.0) : vec3(1.0, 0.0, 1.0);
+      dist = dist2;
+    }
+  }
+  // gl_FragColor.rgb = vec3(shadow * 0.1);
+  gl_FragColor.rgb = uColor * shadow * grain(uv);
+
+  // gl_FragColor.rgb += checker(uv, .1) ? 0.1 : -0.1;
   gl_FragColor.a = 1.0;
 }
 
@@ -108,10 +175,13 @@ void main()	{
 
 const uniforms = {
   uTime: { value: 0 },
+  uZoom: { value: .2 },
   uSize: { value: new THREE.Vector4(window.innerWidth, window.innerHeight, window.innerWidth / window.innerHeight) },
   uParam1: { value: new THREE.Vector4() },
-  uTransform1: { value: new THREE.Vector4(1, 1, 0, 0) },
-  uShadowLength: { value: new THREE.Vector4(16, 8, 1, 1) },
+  uTransform1: { value: new THREE.Vector4(1.7, 1.6, 41.6, 0.8) },
+  uShadow: { value: new THREE.Vector4(7, 1, 1, .33) },
+  uOffset1: { value: new THREE.Vector4(20, .1, 1, 1) },
+  uColor: { value: new THREE.Color('#221c30') },
   uAliasThreshold: { value: 1 },
 }
 
@@ -127,8 +197,25 @@ getPlane({
   })
 })
 
-const localStorage = true
-mnui.vector('transform', uniforms.uTransform1.value, { step: .1, localStorage })
-mnui.vector('shadow length', uniforms.uShadowLength.value, { min: 0, step: .1, localStorage })
-mnui.range('alias threshold', { initialValue: 1 }, { min: 0, max: 10, localStorage })
-  .onUserChange(value => uniforms.uAliasThreshold.value = value)
+const localStorage = mnui.toggle('shader/localStorage (save)', { initialValue: false }, { localStorage: true }).onUserChange(value => {
+  for (const item of mnui.group('shader').children()) {
+    item.localStorage = value
+  }
+}).value
+
+mnui.utils.onFrame(({ time }) => {
+  uniforms.uTime.value = time
+  mnui.group('shader', () => {
+    mnui.range('zoom', uniforms.uZoom.value, [0, 5]).onUserChange(value => uniforms.uZoom.value = value)
+    mnui.vector('transform', uniforms.uTransform1.value, { step: .1, localStorage })
+    mnui.vector('shadow (length, alpha)', uniforms.uShadow.value, { min: 0, step: .1, localStorage })
+    mnui.vector('offset', uniforms.uOffset1.value, { step: .1, localStorage })
+    mnui.range('alias threshold', { initialValue: 1 }, { min: 0, max: 10, localStorage })
+      .onUserChange(value => uniforms.uAliasThreshold.value = value)
+  })
+})
+
+mnui.utils.onDrag(renderer.domElement, info => {
+  uniforms.uTransform1.value.z += info.positionDelta.x * .006 * uniforms.uZoom.value
+  uniforms.uTransform1.value.w += -info.positionDelta.y * .006 * uniforms.uZoom.value
+})
