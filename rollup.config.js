d245536ab72e0f3fce13ecd903bf7e58b60b0ae5
jniac <joseph.merdrignac@gmail.com>
2022-09-28 00:13:01 +0200

    terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
+import { terser } from 'rollup-plugin-terser'
 
 export default [{
   input: 'src/index.ts',
   output: {
     dir: 'dist',
-    format: 'module'
+    format: 'module',
   },
-  plugins: [typescript({ sourceMap: true })]
+  plugins: [
+    typescript(), 
+    terser(),
+  ],
 }, {
   input: 'dist/types/index.d.ts',
   output: [{ file: 'dist/index.d.ts', format: 'es' }],
