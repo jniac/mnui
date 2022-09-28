6fc5e2e2a8710f535905f5095a7a16d968ea7eae
jniac <joseph.merdrignac@gmail.com>
2022-09-28 17:39:25 +0200

    new config (ES2020 / ES2015 minified / d.ts)

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,34 @@
-import typescript from '@rollup/plugin-typescript'
-import dts from 'rollup-plugin-dts'
-import { terser } from 'rollup-plugin-terser'
+import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
+import { terser } from "rollup-plugin-terser"
 
-export default [{
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'module',
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: 'dist/output/mnui.js'
+    },
+    plugins: [
+      typescript({ target: "ES2022", declarationDir: 'dist/mdr' }),
+    ],
   },
-  plugins: [
-    typescript(), 
-    terser(),
-  ],
-}, {
-  input: 'dist/types/index.d.ts',
-  output: [{ file: 'dist/index.d.ts', format: 'es' }],
-  plugins: [dts()],
-}]
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/mnui.es2015.min.js",
+      format: "module",
+    },
+    plugins: [
+      typescript({ target: "ES2015", declaration: false }),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/output/index.d.ts",
+    output: { 
+      file: "dist/mnui.d.ts", 
+      format: "es" 
+    },
+    plugins: [dts()],
+  },
+]
